4909072b241014ef8144a6a328eabd8f42c882c1
Alex Rudenko <alexrudenko@chromium.org>
2022-10-19 16:22:25 +0200

    bidi stats

--- preprocess-data.js
+++ preprocess-data.js
@@ -4,13 +4,15 @@ async function main() {
   const files = fs.readdirSync('./data');
   const data = files.filter(file => file.endsWith('.json')).sort().map(file => {
     const stats = JSON.parse(fs.readFileSync('./data/' + file, 'utf-8')).stats;
+    // Since few tests are supported we don't report the skipped tests to make
+    // sure the chart is readable.
     const counts = {
       passing: stats.passes,
       failing: stats.failures,
-      skipping: stats.pending,
+      skipping: 0, // stats.pending,
       total: stats.tests,
     }
-    counts.unsupported = counts.total - (counts.passing + counts.failing + counts.skipping);
+    counts.unsupported = 0; // counts.total - (counts.passing + counts.failing + counts.skipping);
     return {
       date: Number(file.split('.').shift()) * 1000,
       counts,
