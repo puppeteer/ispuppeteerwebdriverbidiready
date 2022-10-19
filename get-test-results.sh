8fdc7f67f6f1b46864ba9bf8e664e17d508e007d
Alex Rudenko <alexrudenko@chromium.org>
2022-10-19 15:38:19 +0200

    Download firefox

--- get-test-results.sh
+++ get-test-results.sh
@@ -6,9 +6,7 @@ cd $(mktemp -d)
 
 git clone --depth 1 https://github.com/puppeteer/puppeteer.git
 cd puppeteer
-git pull
-git checkout save-test-stats
-npm ci
+PUPPETEER_PRODUCT=firefox npm ci
 npm run build
 npm run test -- --test-suite firefox-bidi --no-coverage --save-stats-to ./stats.json
 
