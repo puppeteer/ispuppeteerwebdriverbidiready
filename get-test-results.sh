b467a53b8383e2ba72fcfb795b58a9e15398dda2
Nikolay Vitkov <34244704+Lightning00Blade@users.noreply.github.com>
2023-05-25 11:53:33 +0200

    chore: use pinned mapper version (#16)

--- get-test-results.sh
+++ get-test-results.sh
@@ -8,13 +8,15 @@ timestamp=$(date +%s)
 
 git clone --depth 1 https://github.com/puppeteer/puppeteer.git
 cd puppeteer
-npm ci
-npm run build
-npm ci
-PUPPETEER_PRODUCT=firefox npm ci
+PUPPETEER_SKIP_DOWNLOAD=true npm ci
+npm run build -w @puppeteer-test/test
+
+PUPPETEER_PRODUCT=firefox node packages/puppeteer/install.js
 
 npm run test -- --test-suite firefox-bidi --no-coverage --save-stats-to ./stats.json
 cp ./stats.json $CWD/data/firefox-$timestamp.json
+
+export PUPPETEER_EXECUTABLE_PATH=$(node tools/download_chrome_bidi.mjs ~/.cache/puppeteer/chrome-canary | cut -d' ' -f 3 | tr -d " \t\n\r" )
 
 npm run test -- --test-suite chrome-bidi --no-coverage --save-stats-to ./stats.json
 cp ./stats.json $CWD/data/chrome-$timestamp.json
