40b5a8ab042cc9f4ef31566d5feab40297ca6ec5
Alex Rudenko <alexrudenko@chromium.org>
2024-07-15 12:59:03 +0200

    ci: fix build

--- get-test-results.sh
+++ get-test-results.sh
@@ -8,7 +8,7 @@ timestamp=$(date +%s)
 
 git clone --depth 1 https://github.com/puppeteer/puppeteer.git
 cd puppeteer
-PUPPETEER_SKIP_DOWNLOAD=true npm ci
+npm ci
 npm run build -w @puppeteer-test/test
 
 PUPPETEER_PRODUCT=firefox node packages/puppeteer/install.mjs
@@ -23,7 +23,6 @@ bidi-firefox() {
 }
 
 bidi-chrome() {
-  export PUPPETEER_EXECUTABLE_PATH=$(node tools/download_chrome_bidi.mjs ~/.cache/puppeteer/chrome-canary --shell)
   npm run test -- --test-suite chrome-bidi --no-coverage --save-stats-to $CWD/data/chrome-$timestamp.json
 }
 
