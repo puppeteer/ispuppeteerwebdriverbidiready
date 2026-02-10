55a730dd12cefeebbc4e7f949108c4873586a0e4
Maksim Sadym <sadym@chromium.org>
2026-02-10 10:31:25 +0100

    renames

--- run-tests.sh
+++ run-tests.sh
@@ -16,18 +16,22 @@ PUPPETEER_PRODUCT=firefox node packages/puppeteer/install.mjs
 export CI=true
 
 bidi-firefox() {
+  echo "Running bidi-firefox"
   npm run test -- --test-suite firefox-headless --no-coverage --save-stats-to $CWD/data/firefox-$timestamp.json --no-cdp-tests
 }
 
 bidi-chrome() {
+  echo "Running bidi-chrome"
   npm run test -- --test-suite chrome-bidi --no-coverage --save-stats-to $CWD/data/chrome-$timestamp.json
 }
 
 bidi-only-chrome() {
+  echo "Running bidi-only-chrome"
   npm run test -- --test-suite chrome-bidi-only --no-coverage --save-stats-to $CWD/data/chromeBidiOnly-$timestamp.json
 }
 
 cdp-chrome() {
+  echo "Running cdp-chrome"
   npm run test -- --test-suite chrome-headless --no-coverage --save-stats-to $CWD/data/chrome-cdp-$timestamp.json
 }
 
